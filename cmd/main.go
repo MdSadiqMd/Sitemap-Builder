ccaaaaabf0b6e0413adb45f2fb2111407ff4cc00
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-07-07 00:19:49 +0530

    get function modularised

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 
 	"github.com/MdSadiqMd/Sitemap-Builder/pkg"
 )
@@ -15,26 +11,7 @@ func main() {
 	urlFlag := flag.String("url", "https://google.com", "The url for which we want to build sitemap for")
 	flag.Parse()
 
-	resp, err := http.Get(*urlFlag)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	bodyReader := bytes.NewReader(bodyBytes)
-	reqUrl := resp.Request.URL
-	baseUrl := &url.URL{
-		Scheme: reqUrl.Scheme,
-		Host:   reqUrl.Host,
-	}
-
-	base := baseUrl.String()
-	pages := pkg.Hrefs(bodyReader, base)
+	pages := pkg.Get(*urlFlag)
 	for _, page := range pages {
 		fmt.Println(page)
 	}
---
+++ pkg/get.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func Get(urlString string) []string {
+	resp, err := http.Get(urlString)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	bodyReader := bytes.NewReader(bodyBytes)
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+
+	base := baseUrl.String()
+	pages := Hrefs(bodyReader, base)
+	return pages
+}
