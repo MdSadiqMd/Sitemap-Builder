d12725c01447b8c135a1e732b39135cccdff5ed7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-07-07 00:03:21 +0530

    parser done'

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	href "github.com/MdSadiqMd/Href-Parser/pkg"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	urlFlag := flag.String("url", "https://google.com", "The url for which we want to build sitemap for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	bodyReader := bytes.NewReader(bodyBytes)
+	links, _ := href.Parse(bodyReader)
+	for _, l := range links {
+		fmt.Println(l)
+	}
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+	fmt.Println(base)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/MdSadiqMd/Sitemap-Builder
 
 go 1.23.3
+
+require github.com/MdSadiqMd/Href-Parser v0.0.0-20250704195124-e885a0a8952c
+
+require golang.org/x/net v0.41.0 // indirect
